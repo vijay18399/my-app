a6d5fcc40d3f3911323d6e847b62daf212f8963c
vijay18399 <vijayreddy18399@gmail.com>
2024-04-15 23:23:36 +0530

    hash routing added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import { Routes,Route} from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import TodoApp from "./pages/TodoApp/TodoApp";
 import WeatherApp from "./pages/WeatherApp/WeatherApp";
@@ -12,22 +13,23 @@ import NumberLogin from './pages/NumberLogin/NumberLogin';
 import './index.css';
  const App = ()=> {
         return (
-            <Router>
-                <div className="App">
-                    <Routes>
-                        <Route path="/"  element={<Home />}></Route>
-                        <Route path="/todo-app"  element={<TodoApp />}></Route>
-                        <Route path="/weather-app"  element={<WeatherApp />}></Route>
-                        <Route path="/auth-app"  element={<AuthApp />}></Route> 
-                        <Route path="/formic-form"  element={<FormikForm />}></Route>  
-                        <Route path="/dictionary"  element={<DictionaryApp />}></Route>   
-                        <Route path="/readme"  element={<ReadMe />}></Route>  
-                        <Route path="/login"  element={<NumberLogin />}></Route>   
-                    </Routes>
-                </div>
+            <Router >
+                    <div className="App">
+                        <Routes>
+                            <Route path="/"  element={<Home />}></Route>
+                            <Route path="/todo-app"  element={<TodoApp />}></Route>
+                            <Route path="/weather-app"  element={<WeatherApp />}></Route>
+                            <Route path="/auth-app"  element={<AuthApp />}></Route> 
+                            <Route path="/formic-form"  element={<FormikForm />}></Route>  
+                            <Route path="/dictionary"  element={<DictionaryApp />}></Route>   
+                            <Route path="/readme"  element={<ReadMe />}></Route>  
+                            <Route path="/login"  element={<NumberLogin />}></Route>   
+                        </Routes>
+                    </div>
             </Router>
-        );
+        ); 
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(<App />);
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,8 +6,6 @@ import { FaWpforms } from "react-icons/fa6";
 import { RiEnglishInput } from "react-icons/ri";
 import { FaReadme } from "react-icons/fa";
 import { FaUserLock } from "react-icons/fa";
-import { GiCookingPot } from "react-icons/gi";
-import { FiActivity } from "react-icons/fi";
 import { IoIosChatboxes } from "react-icons/io";
 import { FaRegMoneyBillAlt } from "react-icons/fa";
 import { ToastContainer, toast } from 'react-toastify';
@@ -20,68 +18,68 @@ import styles from "./Home.module.css";
 const apps = [
   {
     name: "Todo App",
-    path: "/todo-app",
+    path: "todo-app",
     status : "completed",
     icon: <LuListTodo />,
     description: "A simple application for managing tasks and to-do lists."
   },
   {
     name: "Weather App",
-    path: "/weather-app",
+    path: "weather-app",
     status : "completed",
     icon: <TiWeatherSunny />,
     description: "An application for checking the weather forecast in different locations."
   },
   {
     name: "Auth App",
-    path: "/auth-app",  
+    path: "auth-app",  
     status : "completed",
     icon: <FaUserLock />,
     description: "An authentication application for user login and registration."
   },
   {
     name: "Formic App",
-    path: "/formic-form",
+    path: "formic-form",
     status : "completed",
     icon: <FaWpforms />,
     description: "A form management application for creating and handling forms."
   },
   {
     name: "Dictionary App",
-    path: "/dictionary",
+    path: "dictionary",
     status : "completed",
     icon: <RiEnglishInput />,
     description: "An app for looking up word meanings and definitions."
   },
   {
     name: "Readme App",
-    path: "/readme",
+    path: "readme",
     status : "completed",
     icon: <FaReadme />,
     description: "An application for reading practice."
   },
   {
     name: "Number Login",
-    path: "/login",
+    path: "login",
     status : "wip",
     icon: <FaRegMoneyBillAlt />,
     description: "OTP inputs implementation"
   },
   {
     name: "Blog App",
-    path: "/blog-app",
+    path: "blog-app",
     icon: <FaBlog />,
     description: "A blog application for reading, writing, and interacting with posts."
   },
   {
     name: "E-commerce App",
-    path: "/e-commerce-app",
+    path: "e-commerce-app",
     icon: <FaShoppingCart />,
     description: "An e-commerce platform for buying and selling products online."
   },
   {
     name: "Chat App",
-    path: "/chat-app",
+    path: "chat-app",
     icon: <IoIosChatboxes />,
     description: "A real-time chat application for communicating with others."
   }
@@ -90,7 +88,7 @@ const apps = [
 const Main = () => {
   const handleAppClick = (path, status) => {
     if (status === "completed" || status === "wip") {
-      window.open(path, "_blank");
+      window.open(`/#/${path}`, "_blank");
     } else {
       toast.warning("This app is under construction. Stay tuned for updates!");
     }
